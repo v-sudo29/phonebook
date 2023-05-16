b9196747e8cb7c52ed0bf61f4ebdcf59b68b5ecc
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-05-16 13:12:08 -0700

    Update env variable

--- server/server.js
+++ server/server.js
@@ -121,6 +121,6 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port: ${PORT}`)
