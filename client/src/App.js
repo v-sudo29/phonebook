4df892534a7dad93e0334e32761fcbeedb63cf81
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-05-16 13:04:03 -0700

    Create client frontend

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+
+function App() {
+  
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api/persons").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
+  return (
+    <div>
+
+      {(typeof backendData === undefined) 
+        ? <h1>Loading...</h1>
+        : backendData.map((person, id) => {
+          return (
+            <p key={id}>{person.name}</p>
+          )
+        })
+      }
+
+    </div>
+  )
+}
+
+export default App
